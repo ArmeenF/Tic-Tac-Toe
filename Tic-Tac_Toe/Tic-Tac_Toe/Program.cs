ef8d5b5aab743269665103a5dd2fe416cbff86da
armeenf <armeenfarange@yahoo.com>
2020-10-22 20:26:05 -0700

    board creation

--- Tic-Tac_Toe/Tic-Tac_Toe/Program.cs
+++ Tic-Tac_Toe/Tic-Tac_Toe/Program.cs
@@ -1,11 +1,12 @@
 ﻿using System;
+using System.Runtime.CompilerServices;
 using System.Threading;
 
 namespace Tic_Tac_Toe
 {
     class Program
     {
-        static char[] arr = {'0','1','2','3','4','5','6','7','8','9' };
+        static char[] arr = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
         static int player = 1;
         static int choice;
         static int flag = 0;
@@ -16,7 +17,7 @@ namespace Tic_Tac_Toe
                 Console.Clear();// whenever loop will be again start then screen will be clear  
                 Console.WriteLine("Player1:X and Player2:O");
                 Console.WriteLine("\n");
-                if(player % 2 == 0)//checking the chance of the player
+                if (player % 2 == 0)//checking the chance of the player
                 {
                     Console.WriteLine("Player 2 chance");
                 }
@@ -43,15 +44,48 @@ namespace Tic_Tac_Toe
                 }
                 else//Error handiling
                 {
-                    Console.WriteLine("Sorry the row {0} is already marked with {1}",choice,arr[choice]);
+                    Console.WriteLine("Sorry the row {0} is already marked with {1}", choice, arr[choice]);
                     Console.WriteLine("\n");
                     Console.WriteLine("Please wait 2 seconds while the board reloads");
                     Thread.Sleep(2000);
                 }
                 //flag = CheckWin();//Calling check win
             }
-            while (flag != 1 || flag != -1);
-            
+            while (flag != 1 || flag != -1);// This loof will be run until all cell of the grid is not marked with X and O or some player is not win  
+
+            Console.Clear();// clearing the console 
+            Board();// getting filled board again 
+
+            if (flag == 1)
+            {
+                Console.WriteLine("Player {0} has won", (player % 2) + 1);
+            }
+            else// if flag value is -1 the match will be draw and no one is winner  
+            {
+                Console.WriteLine("Draw");
+            }
+            Console.ReadLine();
+        }
+
+        private static void Board()
+        {
+            Console.WriteLine("     |     |      ");
+
+            Console.WriteLine("  {0}  |  {1}  |  {2}", arr[1], arr[2], arr[3]);
+
+            Console.WriteLine("_____|_____|_____ ");
+
+            Console.WriteLine("     |     |      ");
+
+            Console.WriteLine("  {0}  |  {1}  |  {2}", arr[4], arr[5], arr[6]);
+
+            Console.WriteLine("_____|_____|_____ ");
+
+            Console.WriteLine("     |     |      ");
+
+            Console.WriteLine("  {0}  |  {1}  |  {2}", arr[7], arr[8], arr[9]);
+
+            Console.WriteLine("     |     |      ");
         }
     }
 }
